cab00f14183767c611c6d5dc6e540b2040e371ea
James Keough (via WeWeb) <jim@leadgeneration.com>
2025-04-02 08:50:49 -0400

    v1 - v1

---
+++ src/_front/router.js
@@ -0,0 +1,208 @@
+import { createRouter, createWebHistory } from 'vue-router';
+
+import wwPage from './views/wwPage.vue';
+
+import { initializeData, initializePlugins, onPageUnload } from '@/_common/helpers/data';
+
+let router;
+const routes = [];
+
+function scrollBehavior(to) {
+    if (to.hash) {
+        return {
+            el: to.hash,
+            behavior: 'smooth',
+        };
+    } else {
+        return { top: 0 };
+    }
+}
+
+ 
+/* wwFront:start */
+import pluginsSettings from '../../plugins-settings.json';
+
+// eslint-disable-next-line no-undef
+window.wwg_designInfo = {"id":"76d9003e-3ce7-4fd4-95d3-159c06794981","homePageId":"59b165ff-a992-48be-9f80-98cf218833d6","authPluginId":null,"baseTag":null,"defaultTheme":"light","langs":[{"lang":"en","default":true}],"background":{},"workflows":[],"pages":[{"id":"59b165ff-a992-48be-9f80-98cf218833d6","linkId":"59b165ff-a992-48be-9f80-98cf218833d6","name":"Home","folder":null,"paths":{"en":"home","default":"home"},"langs":["en"],"cmsDataSetPath":null,"sections":[{"uid":"ad818d71-164e-4c41-b074-91572a5ddf21","sectionTitle":"Header","linkId":"c45601f8-7a5e-423d-a961-0a69320d2b55"},{"uid":"a2825606-1cf2-487e-9cb9-7612a996896f","sectionTitle":"Features","linkId":"7be8f556-8a11-48d1-a03f-4a9a3c1428be"},{"uid":"dba77431-b947-45e4-b199-f25425a25b25","sectionTitle":"Pricing","linkId":"1c883bc3-2900-4cc2-8ea0-8426f6cd04e7"},{"uid":"75a3ef3a-63d7-497f-8f43-f3c6646c1b20","sectionTitle":"Footer","linkId":"f61d97c7-84bd-46d3-b339-9ad8b79117d1"}],"pageUserGroups":[],"title":{"en":"Blank | Start from scratch","fr":"Vide | Commencer à partir de zéro"},"meta":{"desc":{},"keywords":{},"socialDesc":{},"socialTitle":{},"structuredData":{}},"metaImage":""}],"plugins":[]};
+// eslint-disable-next-line no-undef
+window.wwg_cacheVersion = 1;
+// eslint-disable-next-line no-undef
+window.wwg_pluginsSettings = pluginsSettings;
+// eslint-disable-next-line no-undef
+window.wwg_disableManifest = false;
+
+const defaultLang = window.wwg_designInfo.langs.find(({ default: isDefault }) => isDefault) || {};
+
+const registerRoute = (page, lang, forcedPath) => {
+    const langSlug = !lang.default || lang.isDefaultPath ? `/${lang.lang}` : '';
+    let path =
+        forcedPath ||
+        (page.id === window.wwg_designInfo.homePageId ? '/' : `/${page.paths[lang.lang] || page.paths.default}`);
+
+    //Replace params
+    path = path.replace(/{{([\w]+)\|([^/]+)?}}/g, ':$1');
+
+    routes.push({
+        path: langSlug + path,
+        component: wwPage,
+        name: `page-${page.id}-${lang.lang}`,
+        meta: {
+            pageId: page.id,
+            lang,
+            isPrivate: !!page.pageUserGroups?.length,
+        },
+        async beforeEnter(to, from) {
+            if (to.name === from.name) return;
+            //Set page lang
+            wwLib.wwLang.defaultLang = defaultLang.lang;
+            wwLib.$store.dispatch('front/setLang', lang.lang);
+
+            //Init plugins
+            await initializePlugins();
+
+            //Check if private page
+            if (page.pageUserGroups?.length) {
+                // cancel navigation if no plugin
+                if (!wwLib.wwAuth.plugin) {
+                    return false;
+                }
+
+                await wwLib.wwAuth.init();
+
+                // Redirect to not sign in page if not logged
+                if (!wwLib.wwAuth.getIsAuthenticated()) {
+                    window.location.href = `${wwLib.wwPageHelper.getPagePath(
+                        wwLib.wwAuth.getUnauthenticatedPageId()
+                    )}?_source=${to.path}`;
+
+                    return null;
+                }
+
+                //Check roles are required
+                if (
+                    page.pageUserGroups.length > 1 &&
+                    !wwLib.wwAuth.matchUserGroups(page.pageUserGroups.map(({ userGroup }) => userGroup))
+                ) {
+                    window.location.href = `${wwLib.wwPageHelper.getPagePath(
+                        wwLib.wwAuth.getUnauthorizedPageId()
+                    )}?_source=${to.path}`;
+
+                    return null;
+                }
+            }
+
+            try {
+                await import(`@/pages/${page.id.split('_')[0]}.js`);
+                await wwLib.wwWebsiteData.fetchPage(page.id);
+
+                //Scroll to section or on top after page change
+                if (to.hash) {
+                    const targetElement = document.getElementById(to.hash.replace('#', ''));
+                    if (targetElement) targetElement.scrollIntoView();
+                } else {
+                    document.body.scrollTop = document.documentElement.scrollTop = 0;
+                }
+
+                return;
+            } catch (err) {
+                wwLib.$store.dispatch('front/showPageLoadProgress', false);
+
+                if (err.redirectUrl) {
+                    return { path: err.redirectUrl || '404' };
+                } else {
+                    //Any other error: go to target page using window.location
+                    window.location = to.fullPath;
+                }
+            }
+        },
+    });
+};
+
+for (const page of window.wwg_designInfo.pages) {
+    for (const lang of window.wwg_designInfo.langs) {
+        if (!page.langs.includes(lang.lang)) continue;
+        registerRoute(page, lang);
+    }
+}
+
+const page404 = window.wwg_designInfo.pages.find(page => page.paths.default === '404');
+if (page404) {
+    for (const lang of window.wwg_designInfo.langs) {
+        // Create routes /:lang/:pathMatch(.*)* etc for all langs of the 404 page
+        if (!page404.langs.includes(lang.lang)) continue;
+        registerRoute(
+            page404,
+            {
+                default: false,
+                lang: lang.lang,
+            },
+            '/:pathMatch(.*)*'
+        );
+    }
+    // Create route /:pathMatch(.*)* using default project lang
+    registerRoute(page404, { default: true, isDefaultPath: false, lang: defaultLang.lang }, '/:pathMatch(.*)*');
+} else {
+    routes.push({
+        path: '/:pathMatch(.*)*',
+        async beforeEnter() {
+            window.location.href = '/404';
+        },
+    });
+}
+
+let routerOptions = {};
+
+const isProd =
+    !window.location.host.includes(
+        '-staging.' + (process.env.WW_ENV === 'staging' ? process.env.VUE_APP_PREVIEW_URL : '')
+    ) && !window.location.host.includes(process.env.VUE_APP_PREVIEW_URL);
+
+if (isProd && window.wwg_designInfo.baseTag?.href) {
+    let baseTag = window.wwg_designInfo.baseTag.href;
+    if (!baseTag.startsWith('/')) {
+        baseTag = '/' + baseTag;
+    }
+    if (!baseTag.endsWith('/')) {
+        baseTag += '/';
+    }
+
+    routerOptions = {
+        base: baseTag,
+        history: createWebHistory(baseTag),
+        routes,
+    };
+} else {
+    routerOptions = {
+        history: createWebHistory(),
+        routes,
+    };
+}
+
+router = createRouter({
+    ...routerOptions,
+    scrollBehavior,
+});
+
+//Trigger on page unload
+let isFirstNavigation = true;
+router.beforeEach(async (to, from) => {
+    if (to.name === from.name) return;
+    if (!isFirstNavigation) await onPageUnload();
+    isFirstNavigation = false;
+    wwLib.globalVariables._navigationId++;
+    return;
+});
+
+//Init page
+router.afterEach((to, from, failure) => {
+    wwLib.$store.dispatch('front/showPageLoadProgress', false);
+    let fromPath = from.path;
+    let toPath = to.path;
+    if (!fromPath.endsWith('/')) fromPath = fromPath + '/';
+    if (!toPath.endsWith('/')) toPath = toPath + '/';
+    if (failure || (from.name && toPath === fromPath)) return;
+    initializeData(to);
+});
+/* wwFront:end */
+
+export default router;
---
+++ vue.config.js
@@ -0,0 +1,20 @@
+process.env.VUE_APP_VERSION = process.env.npm_package_version;
+
+module.exports = {
+    productionSourceMap: false,
+    outputDir: './dist/',
+    assetsDir: 'public',
+    publicPath: '',
+    // eslint-disable-next-line no-undef
+    pages: {"index":{"entry":"src/_front/main.js","template":"public/front.html","filename":"./index.html","lang":"en","title":"Blank | Start from scratch","cacheVersion":1,"meta":[{"name":"title","content":"Blank | Start from scratch"},{"itemprop":"name","content":"Blank | Start from scratch"},{"name":"twitter:card","content":"summary"},{"name":"twitter:title","content":"Blank | Start from scratch"},{"property":"og:title","content":"Blank | Start from scratch"},{"property":"og:site_name","content":"Blank | Start from scratch"},{"property":"og:type","content":"website"},{"name":"robots","content":"index, follow"}],"scripts":{"head":"\n","body":"\n"},"baseTag":{"href":"/","target":"_self"},"alternateLinks":[{"rel":"alternate","hreflang":"x-default","href":"https://76d9003e-3ce7-4fd4-95d3-159c06794981.weweb-preview.io/"},{"rel":"alternate","hreflang":"en","href":"https://76d9003e-3ce7-4fd4-95d3-159c06794981.weweb-preview.io/"}],"chunks":["chunk-vendors","chunk-common","index"]}},
+    configureWebpack: config => {
+        config.module.rules.push({
+            test: /\.mjs$/,
+            include: /node_modules/,
+            type: 'javascript/auto',
+        });
+        config.performance = {
+            hints: false,
+        };
+    },
+};
